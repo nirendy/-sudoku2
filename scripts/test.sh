2be9f58091e271a91a706b692a3c643c88a379ae
nirendy <nirendy@gmail.com>
2019-04-25 22:09:19 +0300

    test env

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,13 +5,13 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-mkdir -p output
-mkdir -p output/expected
+mkdir -p ../output
+mkdir -p ../output/expected_out
 
 for file in $(ls ../tests); do
     echo "Running test: $file"
     sudoku-console < ../tests/"$file" > ../output/"$file".out
-    if diff ../output/"$file".out ../output/expected/"$file".exp; then
+    if diff ../output/"$file".out ../output/expected_out/"$file".exp_out; then
         echo -e "Result: ${GREEN}PASS${NC}"
     else
         echo -e "Result: ${RED}FAIL${NC}"
