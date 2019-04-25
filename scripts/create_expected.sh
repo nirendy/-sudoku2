fcebbc69c5fe66ea5534ddb6895234233e6982a9
nirendy <nirendy@gmail.com>
2019-04-25 21:03:08 +0300

    test env

---
+++ scripts/create_expected.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+cd $(dirname $0)/..
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+mkdir -p output
+mkdir -p output/expected
+
+for file in $(ls tests); do
+    echo "Running test: $file"
+    ../question/sudoku-console < tests/"$file" > output/expected/"$file".exp
+done
